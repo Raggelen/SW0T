fada7e4a63761102333c817bce5654278f77bdbb
Ya Boi Not Rens <martijnbouius@gmail.com>
2017-05-18 10:48:29 +0200

    Add files via upload

---
+++ Main.hx
@@ -0,0 +1,37 @@
+package;
+
+import flixel.FlxGame;
+import flixel.FlxState;
+import states.State_Game;
+import states.State_Menu;
+
+import openfl.display.Sprite;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+
+import flixel.FlxG;
+import flixel.util.FlxColor;
+
+import flixel.tile.FlxTilemap;
+import flixel.FlxObject;
+import flixel.ui.FlxButton;
+import flixel.text.FlxText;
+
+
+
+class Main extends Sprite
+{
+	
+	
+
+	public function new()
+	{
+		super();
+		addChild(new FlxGame(1280, 720, State_Menu)); //sets screen size to 320x180 and sets initial state
+
+		FlxG.camera.bgColor = FlxColor.RED; // Turns screen red
+	}
+
+	
+}
---
+++ Spelers.hx
@@ -0,0 +1,66 @@
+package;
+
+import openfl.display.Sprite;
+
+/**
+ * ...
+ * @author Ramon Boelens
+ */
+
+class Spelers extends Sprite
+{
+	public var playerName : String;
+	public var units : Array<Unit> = new Array();
+	public var hasTurn : Bool = false;
+	var tempType : String;
+	var imagePath : String;
+	
+	public function new() 
+	{
+		super();
+		assignUnitType();
+	}
+	/**
+	 * Giving all the units their class
+	 */
+	function assignUnitType() {		
+		for (i in 0 ... 3) {
+			if (i == 0) {
+				tempType = "light";
+			}
+			else if (i == 1) {
+				tempType = "medium";
+			}
+			else if (i == 2) {
+				tempType = "heavy";
+			}
+			assignUnits(tempType);
+		}
+	}
+	
+	/**
+	 * Giving the units their proper values and storing them in an array called units
+	 * @param	tempType
+	 */
+	function assignUnits(tempType : String) {		
+		if (tempType == "light") {
+			imagePath = "img/Light.png";
+		}
+		else if (tempType == "medium") {
+			imagePath = "img/Medium.png";
+		}
+		else if (tempType == "heavy") {
+			imagePath = "img/Heavy.png";
+		}
+		else {
+			trace ("No such type");
+		}
+		
+		var unit : Unit = new Unit(tempType, true, 1, imagePath);
+		unit.type = tempType;
+		unit.alive = true;
+		unit.position = 1;
+		unit.imagePath = imagePath;
+		units.push(unit);
+	}	
+}
